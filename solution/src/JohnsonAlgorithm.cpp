ba575d7ffd7bab8bd46b3c71036b7dad88c4383e
Okorokov Nikita <okorokovn13@gmail.com>
2025-03-02 23:23:54 +0300

    Added comments in JohnsonAlgo

--- solution/src/JohnsonAlgorithm.cpp
+++ solution/src/JohnsonAlgorithm.cpp
@@ -1,16 +1,17 @@
 #include <JohnsonAlgorithm.h>
 #include <climits>
 
+// Algoroithm to find shortest path
 std::vector<Vertex> JohnsonAlgorithm::find_shortest_path(const Graph& graph_, const Vertex& start, const Vertex& finish) {
-    Graph graph = graph_;
+    Graph graph = graph_; // new graph to add extra vertex
     const auto& vertixes = graph.get_vertices();
     
     BellmanFordAlgorithm bellmanFordAlgorithm;
-    std::vector<std::vector<Vertex>> paths;
+    std::vector<std::vector<Vertex>> paths; // paths from extra vertex to all in graph
     for (const auto& v : vertixes)
-    paths.push_back(bellmanFordAlgorithm.find_shortest_path(graph, start, v));
+        paths.push_back(bellmanFordAlgorithm.find_shortest_path(graph, start, v));
 
-    std::unordered_map<Vertex, int> path_distances;
+    std::unordered_map<Vertex, int> path_distances; // distances of paths from extra vertex to all in graph
     for (const auto& path : paths) {
         long long int sum = 0;
         for (int i = 0; i < (int) path.size() - 1; ++i)
@@ -19,10 +20,10 @@ std::vector<Vertex> JohnsonAlgorithm::find_shortest_path(const Graph& graph_, co
     }
     
     Graph new_graph;
-    const auto& edges = graph.get_edges();
+    const auto& edges = graph.get_edges(); // getting all edges for iterating
     for (const auto& v : vertixes) {
-        for (const auto& edge : graph.get_neighbor_edges(v)) {
-            int new_weight = edge.get_weight() + path_distances[v] - path_distances[edge.get_destination()];
+        for (const auto& edge : graph.get_outgoing_edges(v)) {
+            int new_weight = edge.get_weight() + path_distances[v] - path_distances[edge.get_destination()]; // reweighting edges
             new_graph.add_edge(Edge(v, edge.get_destination(), edge.get_weight()));
         }
     }
